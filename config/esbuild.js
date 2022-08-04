cb325cfadbc944113016efc0538d29bbfabb08af
JuanSanchezKZ <juansanchezkz@DESKTOP-JOGCHN1.localdomain>
2022-08-04 15:57:01 -0300

    first commit

---
+++ config/esbuild.js
@@ -0,0 +1,11 @@
+import esbuild from 'esbuild'
+
+esbuild.build({
+    entryPoints:['src/sw.js', 'src/scripts/scrapper.js'],
+    watch:true,
+    bundle:true,
+    outdir: 'dist',
+    minify: true,
+    allowOverwrite: true
+}).then(response => console.log(JSON.stringify(response)))
+.catch(err => console.log(err))
